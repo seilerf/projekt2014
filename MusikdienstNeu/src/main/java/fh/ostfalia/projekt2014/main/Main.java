771783cb06b62babdd96f97c2c4d55596ab0ab0a
Florian Seiler <f.seiler@ostfalia.de>
2014-02-20 17:46:10 +0100

    Main.java für die Persistenz angepasst

--- MusikdienstNeu/src/main/java/fh/ostfalia/projekt2014/main/Main.java
+++ MusikdienstNeu/src/main/java/fh/ostfalia/projekt2014/main/Main.java
@@ -7,8 +7,12 @@
 package fh.ostfalia.projekt2014.main;
 
 import fh.ostfalia.projekt2014.dao.Mp3Dao;
+import fh.ostfalia.projekt2014.model.Mp3;
 import fh.ostfalia.projekt2014.rmi.Mp3DaoRmi;
 import java.rmi.RemoteException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
@@ -17,14 +21,22 @@ import java.rmi.RemoteException;
 public class Main {
     
     public static void main (String[] args){
-        Mp3Dao mp3Dao = new Mp3Dao();
+        //Mp3Dao mp3Dao = new Mp3Dao();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Mp3");
+        EntityManager em = emf.createEntityManager();
         
-        try {
-            Mp3DaoRmi mp3DaoRmi = new Mp3DaoRmi(mp3Dao);
-            mp3DaoRmi.registerRMI();
-        } catch (RemoteException ex) {
-            System.err.println("RemoteException!");
-        }
+        em.getTransaction().begin();
+        
+        Mp3 mp3 = new Mp3();
+        mp3.setMp3Artist(null);
+        mp3.setMp3_file(null);
+        mp3.setMp3_title("Testtitel");
+        
+        em.close();
+        emf.close();
+       
+        //Mp3DaoRmi mp3DaoRmi = new Mp3DaoRmi(mp3Dao);
+        //mp3DaoRmi.registerRMI();
     }
     
 }
