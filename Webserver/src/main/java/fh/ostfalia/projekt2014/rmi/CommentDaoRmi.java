30801874cae0ad34ac2a1c55fefa9be53bc257c9
Maxwell89 <max.ullmann@gmx.de>
2014-02-25 01:00:51 +0100

    some Fixes -> Finish for today

---
+++ Webserver/src/main/java/fh/ostfalia/projekt2014/rmi/CommentDaoRmi.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fh.ostfalia.projekt2014.rmi;
+
+import java.io.Serializable;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.List;
+
+/**
+ *
+ * @author AdminMax
+ */
+public class CommentDaoRmi implements IntfComment, Serializable {
+
+    private IntfComment intfCommentService;
+    private String serveraddress;
+    
+    public CommentDaoRmi() {
+        super();
+        this.lookupRMI();
+    }
+    
+    public CommentDaoRmi(IntfComment intfCommentService) {
+        this.intfCommentService = intfCommentService;
+        this.lookupRMI();
+    }
+            
+    private void lookupRMI() {
+      try {
+           
+            Registry registry = LocateRegistry.getRegistry("localhost", 1088);
+            System.out.println("Registry erkannt");
+            this.intfCommentService = (IntfComment) registry.lookup("AccessToComment");
+            System.out.println("Lookup für das MusikInterface ausgeführt");
+
+        } catch (RemoteException ex) {
+            System.err.println("RemotExeption beim RMI-Lookup aufgetreten (Klasse: "+ getClass().getName() +")");
+        } catch (NotBoundException ex) {
+            System.err.println("NotBoundException aufgetreten. Objekt nicht gefunden.");;
+        }  
+    }
+    
+    @Override
+    public String getTest() {
+        return intfCommentService.getTest();
+    }
+
+    @Override
+    public void addComment(String comment_Title, String comment_Description) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteCpmment(Integer comment_ID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String[] getComment(Integer comment_ID) {
+        this.lookupRMI();
+        String[] comment = intfCommentService.getComment(comment_ID);
+        System.out.println("Comment_Title: " + comment[1]);
+        return intfCommentService.getComment(comment_ID);
+    }
+
+    @Override
+    public List<String[]> getAllComment() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ Webserver/src/main/java/fh/ostfalia/projekt2014/rmi/IntfComment.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fh.ostfalia.projekt2014.rmi;
+
+import java.util.List;
+
+/**
+ *
+ * @author AdminMax
+ */
+public interface IntfComment {
+    
+    public String getTest();
+    public void addComment(String comment_Title, String comment_Description);
+    public void deleteCpmment(Integer comment_ID);
+    public String[] getComment(Integer comment_ID);
+    public List<String[]> getAllComment();
+}
